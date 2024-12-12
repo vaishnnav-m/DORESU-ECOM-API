8db0fefeb5a6b81f3908ad083046df500d36c3c9
Vaishnnav M <vaishnnav58@gmail.com>
2024-12-12 19:56:19 +0530

    small upate

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const path = require('path')
 
+
+const allowedOrigins = ['https://doresu.vercel.app',"http://localhost:5173/"];
 // enabling cors
 const corsOptions = {
-  origin:'https://doresu.vercel.app',
+  origin: function (origin,callback){
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 }
 app.use(cors(corsOptions))
